7b742a09fab45654ef6102dc4ce7c7b7332c5a7c
ArseniiS <publ.mailbox@gmail.com>
2026-02-11 03:56:50 +0100

    Update Help screen paddings

--- app/src/main/java/org/thoughtcrime/securesms/help/refactor/HelpScreen.kt
+++ app/src/main/java/org/thoughtcrime/securesms/help/refactor/HelpScreen.kt
@@ -8,10 +8,11 @@ package org.thoughtcrime.securesms.help.refactor
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -35,7 +36,6 @@ import org.signal.core.ui.compose.Scaffolds
 import org.signal.core.ui.compose.SignalIcons
 import org.signal.core.ui.compose.horizontalGutters
 import org.thoughtcrime.securesms.R
-import org.thoughtcrime.securesms.compose.rememberStatusBarColorNestedScrollModifier
 
 @Composable
 fun HelpScreen(
@@ -47,94 +47,110 @@ fun HelpScreen(
     onNavigationClick = callbacks::onNavigationClick,
     navigationIcon = SignalIcons.ArrowStart.imageVector,
   ) { paddingValues ->
-    LazyColumn(
+    Column (
       modifier = Modifier
         .padding(paddingValues)
-        .then(rememberStatusBarColorNestedScrollModifier())
+        .fillMaxSize()
+        .horizontalGutters(),
     ) {
-      item {
-        TextField(
+      Text(
+        text = stringResource(id = R.string.HelpFragment__contact_us),
+        style = MaterialTheme.typography.bodyLarge,
+        color = MaterialTheme.colorScheme.onSurface,
+      )
+
+      TextField(
+        modifier = Modifier
+          .fillMaxWidth()
+          .defaultMinSize(minHeight = 144.dp)
+          .padding(top = 12.dp),
+        value = "",
+        label = { Text(text = stringResource(id = R.string.HelpFragment__tell_us_whats_going_on)) },
+        onValueChange = {
+          // TODO: impl
+        },
+        keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences),
+        singleLine = false,
+      )
+
+      Text(
+        modifier = Modifier
+          .padding(top = 16.dp),
+        text = stringResource(id = R.string.HelpFragment__tell_us_why_youre_reaching_out),
+        style = MaterialTheme.typography.bodyLarge,
+        color = MaterialTheme.colorScheme.onSurface,
+      )
+
+      Text(
+        modifier = Modifier
+          .padding(top = 16.dp),
+        text = stringResource(id = R.string.HelpFragment__how_do_you_feel),
+        style = MaterialTheme.typography.bodyLarge,
+        color = MaterialTheme.colorScheme.onSurface,
+      )
+
+      Column {
+        Rows.ToggleRow(
           modifier = Modifier
-            .fillMaxWidth()
-            .defaultMinSize(minHeight = 144.dp)
-            .padding(top = 16.dp, bottom = 12.dp, start = 20.dp, end = 28.dp),
-          value = "",
-          label = { Text(text = stringResource(id = R.string.HelpFragment__tell_us_whats_going_on)) },
-          onValueChange = {
+            .padding(top = 16.dp),
+          checked = state.includeDebugLog,
+          text = stringResource(R.string.HelpFragment__include_debug_log),
+          onCheckChanged = {
             // TODO: impl
+          }
+        )
+
+        Text(
+          text = buildAnnotatedString {
+            withLink(
+              link = LinkAnnotation.Clickable(
+                "view-debug-log",
+                linkInteractionListener = {
+                  callbacks.onWhatIsDebugLogClick()
+                },
+                styles = TextLinkStyles(style = SpanStyle(color = MaterialTheme.colorScheme.primary))
+              )
+            ) {
+              append(stringResource(R.string.HelpFragment__whats_this))
+            }
           },
-          keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences),
-          singleLine = false,
+          style = MaterialTheme.typography.bodyMedium,
+          color = MaterialTheme.colorScheme.onSurfaceVariant,
         )
       }
 
-      item {
-        Column {
-          Rows.ToggleRow(
-            checked = state.includeDebugLog,
-            text = stringResource(R.string.HelpFragment__include_debug_log),
-            onCheckChanged = {
-              // TODO: impl
+      Spacer(modifier = Modifier.weight(1f))
+
+      Row(
+        modifier = Modifier
+          .fillMaxWidth()
+          .padding(bottom = 16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+      ) {
+        Text(
+          text = buildAnnotatedString {
+            withLink(
+              link = LinkAnnotation.Clickable(
+                "view-faq",
+                linkInteractionListener = {
+                  callbacks.onFaqClick()
+                },
+                styles = TextLinkStyles(style = SpanStyle(color = MaterialTheme.colorScheme.primary))
+              )
+            ) {
+              append(stringResource(R.string.HelpFragment__have_you_read_our_faq_yet))
             }
-          )
+          },
+        )
 
-          Text(
-            modifier = Modifier
-              .horizontalGutters(),
-            text = buildAnnotatedString {
-              withLink(
-                link = LinkAnnotation.Clickable(
-                  "view-debug-log",
-                  linkInteractionListener = {
-                    callbacks.onWhatIsDebugLogClick()
-                  },
-                  styles = TextLinkStyles(style = SpanStyle(color = MaterialTheme.colorScheme.primary))
-                )
-              ) {
-                append(stringResource(R.string.HelpFragment__whats_this))
-              }
-            },
-            style = MaterialTheme.typography.bodyMedium,
-            color = MaterialTheme.colorScheme.onSurfaceVariant,
-          )
-        }
-      }
-
-      item {
-        Row(
-          horizontalArrangement = Arrangement.SpaceBetween,
-          verticalAlignment = Alignment.CenterVertically,
-          modifier = Modifier
-            .fillMaxWidth()
-            .horizontalGutters()
-            .padding(top = 32.dp, bottom = 24.dp)
-        ) {
-          Text(
-            text = buildAnnotatedString {
-              withLink(
-                link = LinkAnnotation.Clickable(
-                  "view-faq",
-                  linkInteractionListener = {
-                    callbacks.onFaqClick()
-                  },
-                  styles = TextLinkStyles(style = SpanStyle(color = MaterialTheme.colorScheme.primary))
-                )
-              ) {
-                append(stringResource(R.string.HelpFragment__have_you_read_our_faq_yet))
-              }
-            },
-            style = MaterialTheme.typography.bodyMedium,
-            color = MaterialTheme.colorScheme.onSurfaceVariant,
-          )
-
-          Buttons.LargeTonal(
-            enabled = false,
-            onClick = {
-              // TODO: impl
-            }
-          ) {
-            Text(text = stringResource(R.string.HelpFragment__next))
+        Buttons.LargeTonal(
+          enabled = false,
+          onClick = {
+            // TODO: impl
           }
+        ) {
+          Text(text = stringResource(R.string.HelpFragment__next))
         }
       }
     }
